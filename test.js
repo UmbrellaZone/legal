6db8f1fe556e802c90f2b64bab3873d02299fd85
PhilKunz <github@philkunz.com>
2016-07-19 23:03:23 +0200

    update to latest tools

--- test.js
+++
@@ -1,6 +0,0 @@
-/// <reference path="./typings/tsd.d.ts" />
-var umbrellaLegal = require("./index.js");
-var path = require("path");
-var umbrellaLegal = require("./index.js");
-var easyserve = require("easyserve");
-easyserve(path.resolve("./test/jade/"));
--- web/jade/readme.md
+++
@@ -1,2 +0,0 @@
-# Umbrella.Zone - legaldocs - jade
-This folder contains jade files that are the basis for a basic imprin theme.
